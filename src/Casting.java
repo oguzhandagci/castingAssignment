dfaf659bb186c21edf6b4443891387f79c476637
Ozzy Dagci <35441651+oguzhandagci@users.noreply.github.com>
2022-05-04 11:03:50 -0400

    correct commit

---
+++ src/Casting.java
@@ -0,0 +1,17 @@
+public class Casting {
+
+    public static void main(String[] args) {
+        System.out.println("First Exercise");
+        double data1 = 112.35;
+        int data2 = (int) data1;
+        System.out.println(data1);
+        System.out.println(data2);
+
+        System.out.println("\nSecond Exercise");
+        String variable1 = "41";
+        int variable2 = Integer.parseInt(variable1);
+        System.out.println("The string value is: "+variable1);
+        System.out.println("The integer value is: "+variable2);
+
+    }
+}
